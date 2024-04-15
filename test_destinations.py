1fcf8d00727e43089d4e9971ea85f8c0bce09870
Prasann Shimpi <prasann@prasanns-mbp.mynetworksettings.com>
2024-04-15 01:01:42 -0700

    tests

---
+++ test_destinations.py
@@ -0,0 +1,249 @@
+from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+
+# class TravelWebsiteTests(unittest.TestCase):
+
+#     def setUp(self):
+#         self.driver = webdriver.Firefox()
+#         self.driver.get("http://127.0.0.1:8000/")
+#         self.driver.set_window_size(1440, 814)
+#         form = self.driver.find_element_by_id("destinations-form")
+#         first_name_field = form.find_element_by_name("firstname")
+#         last_name_field = form.find_element_by_name("lastname")
+#         email_field = form.find_element_by_name("email")
+#         phone_field = form.find_element_by_name("phone")
+#         category_field = form.find_element_by_name("querycategory")
+#         message_field = form.find_element_by_name("queryaddn")
+#         newsletter_checkbox = form.find_element_by_name("newsletter")
+#         first_name_field.send_keys("John")
+#         last_name_field.send_keys("Doe")
+#         email_field.send_keys("example@example.com")
+#         phone_field.send_keys("1234567890")
+#         category_field.send_keys("Regarding Destinations")
+#         message_field.send_keys("This is a test message")
+#         newsletter_checkbox.click()
+#         terms_checkbox = self.driver.find_element_by_css_selector("#defaultCheck2")
+#         terms_checkbox.location_once_scrolled_into_view
+#         sleep(1)
+#         terms_checkbox.click()
+#         submit_button = self.driver.find_element_by_css_selector('#form-submit')
+#         self.driver.execute_script("arguments[0].click();",submit_button)
+#         # Wait for the form submission to be processed
+#         WebDriverWait(self.driver, 10).until(EC.url_contains('/destinations'))
+
+#     def test_presence(self):
+#         japan_radio_button = self.driver.find_element_by_id("radioJapan")
+#         assert japan_radio_button is not None, "Japan radio button does not exist"
+
+#         # img presence
+#         image_element = self.driver.find_element(By.CLASS_NAME, "destination-image")
+#         assert image_element is not None, "Image does not display"
+
+#         # slider buttons
+#         prev = self.driver.find_element(By.CLASS_NAME, "previous")
+#         next = self.driver.find_element(By.CLASS_NAME, "next")
+#         assert prev is not None, "Slider not present"
+#         assert next is not None, "Slider not present"
+
+#         # # packagesButton
+#         packagesButton = self.driver.find_element(By.ID, "packagesButton")
+#         assert packagesButton is not None, "Button does not exists."
+
+#     def test_location(self):
+#         radio_buttons = self.driver.find_elements(By.XPATH, "//input[@type='radio']")
+#         image = self.driver.find_element(By.CLASS_NAME, "destination-image")
+
+#         # Get the location of the radio buttons and the image
+#         radio_buttons_location = radio_buttons[0].location
+#         image_location = image.location
+
+#         # Assert that the radio buttons are above the image
+#         assert radio_buttons_location['y'] < image_location['y'], "Radio buttons are not above the image."
+
+#         image = self.driver.find_element(By.CLASS_NAME, "destination-image")
+#         slider = self.driver.find_element(By.CLASS_NAME, "next")
+
+#         # Get the location of the image and the slider
+#         image_location = image.location
+#         slider_location = slider.location
+
+#         # Assert that the slider is below the image
+#         self.assertGreater(slider_location['y'], image_location['y'], "Slider is not below the image.")
+
+#         slider = self.driver.find_element(By.CLASS_NAME, "next")
+#         packagesButton = self.driver.find_element(By.ID, "packagesButton")
+
+#         # Get the location of the image and the slider
+#         slider_location = slider.location
+#         packagesButton_location = packagesButton.location
+
+#         # Assert that the slider is below the image
+#         self.assertGreater( packagesButton_location['y'],slider_location['y'], "Slider is not above the button.")
+
+
+    
+#     def test_size(self):
+#         element = self.driver.find_element(By.ID, "japan")
+
+#         # Get the location of the element
+#         element_location = element.location
+
+#         # Get the viewport height
+#         viewport_height = self.driver.execute_script("return window.innerHeight")
+
+#         # Calculate 80% of the viewport height
+#         eighty_percent_height = viewport_height * 0.8
+
+#         # Assert that the element is above 80% of the screen size
+#         self.assertLess(element_location['y'], eighty_percent_height, "Element is not above 80% of the screen size.")
+
+#     def test_content(self):
+#         sleep(5)
+#         h1_element = self.driver.find_element_by_xpath("//h1[@class='title']/span[@class='underline']")
+#         text = h1_element.text
+#         assert "Explore" in text
+    
+#     def test_functionality(self):
+#         radio_buttons = self.driver.find_elements_by_css_selector('input[name="flexRadioDefault"]')
+
+#         # Iterate through each radio button
+#         for radio_button in radio_buttons:
+#             # Click on the radio button
+#             radio_button.click()
+#             # Wait for a brief moment for the JavaScript to execute and the country description to appear
+#             sleep(3)
+#             # Check if the selected country description is visible while others are hidden
+#             country_descriptions = self.driver.find_elements_by_css_selector('.country-description')
+#             for description in country_descriptions:
+#                 if radio_button.get_attribute('value').lower() in description.get_attribute('id'):
+#                     assert 'd-none' not in description.get_attribute('class'), f"{radio_button.get_attribute('value')} description is not visible"
+#                 else:
+#                     assert 'd-none' in description.get_attribute('class'), f"{description.get_attribute('id')} description is visible while it should be hidden"
+
+#         # Button press
+#         see_packages_button = self.driver.find_element_by_css_selector('#see-packages-btn')
+#         self.driver.execute_script("arguments[0].click();",see_packages_button)
+#         # Wait for the form submission to be processed
+#         WebDriverWait(self.driver, 10).until(EC.url_contains('/packages'))
+#         assert "/packages" in self.driver.current_url, "Redirection failed"
+
+    
+#     def tearDown(self):
+#         sleep(1)  # Optional delay for better visualization
+#         self.driver.close()
+
+# if __name__ == "__main__":
+#     unittest.main()
+
+
+class TravelWebsiteTests(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.get("http://127.0.0.1:8000/")
+        self.driver.set_window_size(1440, 814)
+        form = self.driver.find_element_by_id("destinations-form")
+        first_name_field = form.find_element_by_name("firstname")
+        last_name_field = form.find_element_by_name("lastname")
+        email_field = form.find_element_by_name("email")
+        phone_field = form.find_element_by_name("phone")
+        category_field = form.find_element_by_name("querycategory")
+        message_field = form.find_element_by_name("queryaddn")
+        newsletter_checkbox = form.find_element_by_name("newsletter")
+        first_name_field.send_keys("John")
+        last_name_field.send_keys("Doe")
+        email_field.send_keys("example@example.com")
+        phone_field.send_keys("1234567890")
+        category_field.send_keys("Regarding Destinations")
+        message_field.send_keys("This is a test message")
+        newsletter_checkbox.click()
+        terms_checkbox = self.driver.find_element_by_css_selector("#defaultCheck2")
+        terms_checkbox.location_once_scrolled_into_view
+        sleep(1)
+        terms_checkbox.click()
+        submit_button = self.driver.find_element_by_css_selector('#form-submit')
+        self.driver.execute_script("arguments[0].click();",submit_button)
+        # Wait for the form submission to be processed
+        WebDriverWait(self.driver, 10).until(EC.url_contains('/destinations'))
+
+    def test_japan_radio_button_existence(self):
+        japan_radio_button = self.driver.find_element_by_id("radioJapan")
+        self.assertIsNotNone(japan_radio_button, "Japan radio button does not exist")
+
+    def test_image_presence(self):
+        image_element = self.driver.find_element(By.CLASS_NAME, "destination-image")
+        self.assertIsNotNone(image_element, "Image does not display")
+
+    def test_slider_buttons_presence(self):
+        prev = self.driver.find_element(By.CLASS_NAME, "previous")
+        next = self.driver.find_element(By.CLASS_NAME, "next")
+        self.assertIsNotNone(prev, "Slider not present")
+        self.assertIsNotNone(next, "Slider not present")
+
+    def test_packages_button_existence(self):
+        packagesButton = self.driver.find_element(By.ID, "packagesButton")
+        self.assertIsNotNone(packagesButton, "Button does not exists.")
+
+    def test_radio_buttons_location(self):
+        radio_buttons = self.driver.find_elements(By.XPATH, "//input[@type='radio']")
+        image = self.driver.find_element(By.CLASS_NAME, "destination-image")
+        radio_buttons_location = radio_buttons[0].location
+        image_location = image.location
+        self.assertLess(radio_buttons_location['y'], image_location['y'], "Radio buttons are not above the image.")
+
+    def test_slider_below_image(self):
+        image = self.driver.find_element(By.CLASS_NAME, "destination-image")
+        slider = self.driver.find_element(By.CLASS_NAME, "next")
+        image_location = image.location
+        slider_location = slider.location
+        self.assertGreater(slider_location['y'], image_location['y'], "Slider is not below the image.")
+
+    def test_button_above_slider(self):
+        slider = self.driver.find_element(By.CLASS_NAME, "next")
+        packagesButton = self.driver.find_element(By.ID, "packagesButton")
+        slider_location = slider.location
+        packagesButton_location = packagesButton.location
+        self.assertGreater(packagesButton_location['y'], slider_location['y'], "Slider is not above the button.")
+
+    def test_element_size(self):
+        element = self.driver.find_element(By.ID, "japan")
+        element_location = element.location
+        viewport_height = self.driver.execute_script("return window.innerHeight")
+        eighty_percent_height = viewport_height * 0.8
+        self.assertLess(element_location['y'], eighty_percent_height, "Element is not above 80% of the screen size.")
+
+    def test_content(self):
+        sleep(5)
+        h1_element = self.driver.find_element_by_xpath("//h1[@class='title']/span[@class='underline']")
+        text = h1_element.text
+        self.assertIn("Explore", text)
+
+    def test_radio_buttons_functionality(self):
+        radio_buttons = self.driver.find_elements_by_css_selector('input[name="flexRadioDefault"]')
+        for radio_button in radio_buttons:
+            radio_button.click()
+            sleep(3)
+            country_descriptions = self.driver.find_elements_by_css_selector('.country-description')
+            for description in country_descriptions:
+                if radio_button.get_attribute('value').lower() in description.get_attribute('id'):
+                    self.assertNotIn('d-none', description.get_attribute('class'), f"{radio_button.get_attribute('value')} description is not visible")
+                else:
+                    self.assertIn('d-none', description.get_attribute('class'), f"{description.get_attribute('id')} description is visible while it should be hidden")
+
+    def test_see_packages_button_functionality(self):
+        see_packages_button = self.driver.find_element_by_css_selector('#packagesButton')
+        self.driver.execute_script("arguments[0].click();",see_packages_button)
+        WebDriverWait(self.driver, 10).until(EC.url_contains('/packages'))
+        self.assertIn("/packages", self.driver.current_url, "Redirection failed")
+
+    def tearDown(self):
+        sleep(1)
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ test_packages_2.py
@@ -0,0 +1,231 @@
+
+from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+import warnings
+
+# class TravelWebsiteTests(unittest.TestCase):
+
+#     def setUp(self):
+#         self.driver = webdriver.Firefox()
+#         self.driver.get("http://127.0.0.1:8000/")
+#         self.driver.set_window_size(1440, 814)
+#         form = self.driver.find_element_by_id("destinations-form")
+#         first_name_field = form.find_element_by_name("firstname")
+#         last_name_field = form.find_element_by_name("lastname")
+#         email_field = form.find_element_by_name("email")
+#         phone_field = form.find_element_by_name("phone")
+#         category_field = form.find_element_by_name("querycategory")
+#         message_field = form.find_element_by_name("queryaddn")
+#         newsletter_checkbox = form.find_element_by_name("newsletter")
+#         first_name_field.send_keys("John")
+#         last_name_field.send_keys("Doe")
+#         email_field.send_keys("example@example.com")
+#         phone_field.send_keys("1234567890")
+#         category_field.send_keys("Regarding Destinations")
+#         message_field.send_keys("This is a test message")
+#         newsletter_checkbox.click()
+#         terms_checkbox = self.driver.find_element_by_css_selector("#defaultCheck2")
+#         terms_checkbox.location_once_scrolled_into_view
+#         sleep(1)
+#         terms_checkbox.click()
+#         submit_button = self.driver.find_element_by_css_selector('#form-submit')
+#         self.driver.execute_script("arguments[0].click();",submit_button)
+#         # Wait for the form submission to be processed
+#         WebDriverWait(self.driver, 10).until(EC.url_contains('/destinations'))
+#         see_packages_button = self.driver.find_element_by_css_selector('#packagesButton')
+#         self.driver.execute_script("arguments[0].click();",see_packages_button)
+#         # Wait for the form submission to be processed
+#         WebDriverWait(self.driver, 10).until(EC.url_contains('/packages'))
+
+#     def test_existence(self):
+#         # Cards
+#         card = self.driver.find_elements_by_css_selector('.card')
+#         assert card is not None, "card is not present on the page"
+#         card_img = self.driver.find_elements_by_css_selector('.card-img-top')
+#         assert card_img is not None, "card_img is not present on the page"
+#         card_body = self.driver.find_elements_by_css_selector('.card-body')
+#         assert card_body is not None, "card_body is not present on the page"
+#         card_text = self.driver.find_elements_by_css_selector('.card-text')
+#         assert card_text is not None, "card_text is not present on the page"
+
+#         # rangeValueLabel
+#         slider = self.driver.find_elements_by_css_selector('#customRange1')
+#         assert slider is not None, "slider is not present on the page"
+
+#         # packageSelect
+#         selectRange = self.driver.find_elements_by_css_selector('#packageSelect')
+#         assert selectRange is not None, "selectRange is not present on the page"
+
+#     def test_location(self):
+#         cards = self.driver.find_elements_by_css_selector('.card')
+#         slider = self.driver.find_element_by_css_selector('#customRange1')
+#         button = self.driver.find_element_by_css_selector('.btn')
+
+#         # Get the locations of the elements
+#         cards_location = cards[0].location
+#         slider_location = slider.location
+#         button_location = button.location
+
+#         # Assert that cards are above slider
+#         assert cards_location['y'] < slider_location['y'], "Cards are not above the slider"
+
+#         # Assert that button is below slider
+#         assert button_location['y'] > slider_location['y'], "Button is not below the slider"
+        
+#     def test_size(self):
+#         image = self.driver.find_element_by_css_selector('.card-img-top')
+
+#         # Get the size of the image
+#         image_size = image.size
+
+#         # Assert that both width and height are greater than zero
+#         assert image_size['width'] > 0 and image_size['height'] > 0, "Image size is zero"
+
+#     def test_content(self):
+#         ul_element = self.driver.find_element_by_css_selector('.list-group')
+
+#         # Get all child elements of the ul
+#         child_elements = ul_element.find_elements_by_css_selector('*')
+
+#         # Assert that the ul has at least one child element
+#         assert len(child_elements) > 0, "The ul element is empty"
+    
+#     def test_functionality(self):
+#         # button = self.driver.find_element_by_css_selector(".btn")
+#         # button.click()
+#         submit_button = self.driver.find_element_by_css_selector('.btn')
+#         self.driver.execute_script("arguments[0].click();",submit_button)
+
+#         # Wait for the alert to appear
+#         sleep(2)  # Adjust the sleep time as needed
+
+#         # Switch to the alert
+#         alert = self.driver.switch_to.alert
+
+#         # Get the text of the alert
+#         alert_text = alert.text
+
+#         # Print or use the alert text as needed
+#         print("Alert Text:", alert_text)
+
+#         # Dismiss the alert (clicking the OK button)
+#         alert.accept()  # or alert.dismiss() if you want to dismiss it
+#         # Cost for selected package is: $4300
+#         assert "Cost for selected package is: $4300" == alert_text, "The ul element is empty"
+
+
+
+
+#     def tearDown(self):
+#         sleep(1)  # Optional delay for better visualization
+#         self.driver.close()
+
+# if __name__ == "__main__":
+#     unittest.main()
+
+from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+class TravelWebsiteTests(unittest.TestCase):
+
+    def setUp(self):
+        warnings.filterwarnings("ignore", category=DeprecationWarning, module='urllib3')
+        self.driver = webdriver.Firefox()
+        self.driver.get("http://127.0.0.1:8000/packages")
+        # self.driver.set_window_size(1440, 814)
+        # form = self.driver.find_element_by_id("destinations-form")
+        # first_name_field = form.find_element_by_name("firstname")
+        # last_name_field = form.find_element_by_name("lastname")
+        # email_field = form.find_element_by_name("email")
+        # phone_field = form.find_element_by_name("phone")
+        # category_field = form.find_element_by_name("querycategory")
+        # message_field = form.find_element_by_name("queryaddn")
+        # newsletter_checkbox = form.find_element_by_name("newsletter")
+        # first_name_field.send_keys("John")
+        # last_name_field.send_keys("Doe")
+        # email_field.send_keys("example@example.com")
+        # phone_field.send_keys("1234567890")
+        # category_field.send_keys("Regarding Destinations")
+        # message_field.send_keys("This is a test message")
+        # newsletter_checkbox.click()
+        # terms_checkbox = self.driver.find_element_by_css_selector("#defaultCheck2")
+        # terms_checkbox.location_once_scrolled_into_view
+        # sleep(1)
+        # terms_checkbox.click()
+        # submit_button = self.driver.find_element_by_css_selector('#form-submit')
+        # self.driver.execute_script("arguments[0].click();", submit_button)
+        # # Wait for the form submission to be processed
+        # WebDriverWait(self.driver, 10).until(EC.url_contains('/destinations'))
+        # see_packages_button = self.driver.find_element_by_css_selector('#packagesButton')
+        # self.driver.execute_script("arguments[0].click();", see_packages_button)
+        # # Wait for the form submission to be processed
+        WebDriverWait(self.driver, 10).until(EC.url_contains('/packages'))
+
+    def test_card_existence(self):
+        card = self.driver.find_elements_by_css_selector('.card')
+        assert card is not None, "card is not present on the page"
+
+    def test_card_img_existence(self):
+        card_img = self.driver.find_elements_by_css_selector('.card-img-top')
+        assert card_img is not None, "card_img is not present on the page"
+
+    def test_card_body_existence(self):
+        card_body = self.driver.find_elements_by_css_selector('.card-body')
+        assert card_body is not None, "card_body is not present on the page"
+
+    def test_card_text_existence(self):
+        card_text = self.driver.find_elements_by_css_selector('.card-text')
+        assert card_text is not None, "card_text is not present on the page"
+
+    def test_slider_existence(self):
+        slider = self.driver.find_elements_by_css_selector('#customRange1')
+        assert slider is not None, "slider is not present on the page"
+
+    def test_package_select_existence(self):
+        selectRange = self.driver.find_elements_by_css_selector('#packageSelect')
+        assert selectRange is not None, "selectRange is not present on the page"
+
+    def test_card_location(self):
+        cards = self.driver.find_elements_by_css_selector('.card')
+        slider = self.driver.find_element_by_css_selector('#customRange1')
+        button = self.driver.find_element_by_css_selector('.btn')
+        cards_location = cards[0].location
+        slider_location = slider.location
+        button_location = button.location
+        assert cards_location['y'] < slider_location['y'], "Cards are not above the slider"
+        assert button_location['y'] > slider_location['y'], "Button is not below the slider"
+
+    def test_image_size(self):
+        image = self.driver.find_element_by_css_selector('.card-img-top')
+        image_size = image.size
+        assert image_size['width'] > 0 and image_size['height'] > 0, "Image size is zero"
+
+    def test_ul_element_content(self):
+        ul_element = self.driver.find_element_by_css_selector('.list-group')
+        child_elements = ul_element.find_elements_by_css_selector('*')
+        assert len(child_elements) > 0, "The ul element is empty"
+
+    def test_alert_content(self):
+        submit_button = self.driver.find_element_by_css_selector('.btn')
+        self.driver.execute_script("arguments[0].click();", submit_button)
+        sleep(2)  # Adjust the sleep time as needed
+        alert = self.driver.switch_to.alert
+        alert_text = alert.text
+        alert.accept()
+        assert "Cost for selected package is: $4300" == alert_text, "The alert content is not as expected"
+
+    def tearDown(self):
+        sleep(1)  # Optional delay for better visualization
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
